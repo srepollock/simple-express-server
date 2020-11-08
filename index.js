0abbf683c433ce01eb5b777e5bc5f2312430e333
Spencer Pollock <spencer@spollock.ca>
2020-11-07 22:43:28 -0800

    fix: 🐛 Updating the router to run both

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const localhost = 'http://localhost';
 const app1 = express()
 const app1_port = 3000;
+app1.use(bodyParser.urlencoded({extended: true}));
+app1.use(bodyParser.json());
+
+
 const app2 = express()
 const app2_port = 3001;
 
-app1.get('/', (req, res) => {
-    res.send('Welcome to the website!');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({message: 'hooray! welcome to the api!'});
 });
 
-app1.listen(app1_port, () => console.log(`Express app listening on port ${app1_port}`));
+app1.use('/api', router);
+
+app1.listen(app1_port, () => console.log(`Express app listening on port ${localhost}:${app1_port}/api`));
 
 app2.use(express.static('public'));
 
-app2.listen(app2_port, () => console.log(`Express app listening on port ${app2_port}`));
+app2.listen(app2_port, () => console.log(`Express app listening on port ${localhost}${app2_port}`));
